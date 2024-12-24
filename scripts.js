2c9872cc9a61ee15a7fe5c40934eef2f4f301078
harsh-bansal03 <harshbansal828@gmail.com>
2024-12-24 21:09:56 +0530

    linking form

--- scripts.js
+++ scripts.js
@@ -31,3 +31,42 @@ function toggleMenu() {
     const links = document.querySelector('.navbar-links');
     links.classList.toggle('active');
 }
+
+
+
+
+//form :
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.forms['contact-form'];
+    const submitBtn = form.querySelector('button[type="submit"]');
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyQMjeuiiGhgty8n9TeeMNt8dGwliA_OYd0fbQNbIDSAezZRz0p0lIBAYrI1qjE-kvL/exec';
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        submitBtn.disabled = true;
+        submitBtn.textContent = 'Submitting...';
+
+        try {
+            await fetch(scriptURL, { method: 'POST', body: new FormData(form) });
+            Swal.fire({
+                title: 'Submission Successful!',
+                text: 'Thank you for joining our community.',
+                icon: 'success',
+                confirmButtonText: 'Okay',
+            });
+            form.reset(); // Reset the form fields after successful submission
+        } catch (error) {
+            console.error('Error!', error.message);
+            Swal.fire({
+                title: 'Error!',
+                text: 'There was a problem submitting the form. Please try again later.',
+                icon: 'error',
+                confirmButtonText: 'Okay',
+            });
+        } finally {
+            submitBtn.disabled = false;
+            submitBtn.textContent = 'Submit';
+        }
+    });
+});
